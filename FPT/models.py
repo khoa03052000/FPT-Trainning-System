16098f5514917cd387ec9afb4aa85d5419210ec5
KhoaFPT <khoahtgcd191049@fpt.edu.vn>
2021-01-18 11:57:45 +0700

    #AD-5 Update field Trainer Model

--- FPT/models.py
+++ FPT/models.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+TYPE_DEPARTMENT = [
+    ('ET', 'External Type'),
+    ('IT', 'Internal Type'),
+]
+
 
 # Create your models here.
 class User(AbstractUser):
@@ -11,6 +16,10 @@ class User(AbstractUser):
 class Trainer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=12)
+    working_place = models.CharField(50)
+    type = models.CharField(max_length=2, choices=TYPE_DEPARTMENT, default='ET')
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
