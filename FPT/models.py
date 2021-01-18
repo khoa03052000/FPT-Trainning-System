2ecd39c4e9499d0acbb07f28ddf3290f0d55f9aa
KhoaFPT <khoahtgcd191049@fpt.edu.vn>
2021-01-18 23:06:05 +0700

    #AD-12 Update All Model

--- FPT/models.py
+++ FPT/models.py
@@ -13,7 +13,7 @@ TYPE_DEPARTMENT = [
 class User(AbstractUser):
     is_trainer = models.BooleanField(default=False)
     is_trainee = models.BooleanField(default=False)
-    department = models.CharField(default="HR", max_length=20)
+    department = models.CharField(default="FPT", max_length=20)
 
 
 class Trainer(models.Model):
@@ -21,9 +21,9 @@ class Trainer(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField()
     phone = models.CharField(max_length=12)
-    working_place = models.CharField(max_length=50)
+    working_place = models.CharField(max_length=50, null=True, blank=True)
     type = models.CharField(max_length=2, choices=TYPE_DEPARTMENT, default='ET')
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -35,12 +35,12 @@ class Trainee(models.Model):
     phone = models.CharField(max_length=12)
     age = models.IntegerField(default=18)
     dot = models.DateField(default="03/05/2000")
-    education = models.CharField(max_length=50)
+    education = models.CharField(max_length=50, null=True, blank=True)
     experience = models.IntegerField(default=1)
-    location = models.CharField(max_length=50)
+    location = models.CharField(max_length=50, null=True, blank=True)
     toeic_score = models.IntegerField(default=1)
-    department = models.CharField(max_length=50)
-    description = models.TextField()
+    department = models.CharField(max_length=50, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -61,7 +61,7 @@ class Course(models.Model):
     assigned_user = GenericForeignKey("assigned_user_type", "assigned_user_id")
     name = models.CharField(max_length=50)
     category = models.ManyToManyField(Category)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     image = models.ImageField()
     is_visible = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
