b530dc696ba32ddca87019ac9a202fafb197efb1
KhoaFPT <khoahtgcd191049@fpt.edu.vn>
2021-01-18 11:49:53 +0700

    #AD-5 Create Trainer Model

--- FPT/models.py
+++ FPT/models.py
@@ -6,3 +6,11 @@ from django.db import models
 class User(AbstractUser):
     is_trainer = models.BooleanField(default=False)
     is_trainee = models.BooleanField(default=False)
+
+
+class Trainer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
