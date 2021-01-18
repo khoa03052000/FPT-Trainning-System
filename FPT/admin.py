952b572e2e53c0ba485742e0a39c80d008f2b983
KhoaFPT <khoahtgcd191049@fpt.edu.vn>
2021-01-18 23:06:32 +0700

    #AD-13 Register Admin Site for User

--- FPT/admin.py
+++ FPT/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import admin as auth_admin
 
 from .models import *
 # Register your models here.
 admin.site.register(Trainee)
-admin.site.register(User, UserAdmin)
 admin.site.register(Trainer)
 admin.site.register(Course)
 admin.site.register(Category)
 admin.site.register(Request)
+
+
+@admin.register(User)
+class UserAdmin(auth_admin.UserAdmin):
+    fieldsets = (
+        (
+            "User",
+            {
+                "fields": (
+                    "is_trainer",
+                    "is_trainee",
+                )
+            },
+        ),
+    ) + auth_admin.UserAdmin.fieldsets
+    list_display = ["username", "is_superuser", "is_staff", "is_trainer", "is_trainee"]
