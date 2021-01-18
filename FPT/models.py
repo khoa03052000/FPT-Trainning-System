acb6d265a06927b18817f59a0897777b9be6f3ac
KhoaFPT <khoahtgcd191049@fpt.edu.vn>
2021-01-18 11:43:16 +0700

    #AD-4 Create User default Model from AbstractUser

--- FPT/models.py
+++ FPT/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class User(AbstractUser):
+    is_trainer = models.BooleanField(default=False)
+    is_trainee = models.BooleanField(default=False)
--- FPT_Trainning/settings.py
+++ FPT_Trainning/settings.py
@@ -70,14 +70,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "FPT_Trainning.wsgi.application"
 
-
+AUTH_USER_MODEL = "FPT.User"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'FPT',
+        'USER':'postgres',
+        'PASSWORD':'1234',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
 
