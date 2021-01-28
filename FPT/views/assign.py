89bd93028832b43a90e5f3447c442da7ee5079e9
KhoaFPT <khoahtgcd191049@fpt.edu.vn>
2021-01-28 11:21:42 +0700

    Finish Assign User

--- FPT/views/assign.py
+++ FPT/views/assign.py
@@ -1,7 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.contenttypes.models import ContentType
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
 
@@ -10,6 +9,7 @@ from FPT.models import Course, Trainer, Trainee, AssignUserToCourse
 
 @require_http_methods(["GET"])
 @login_required
+# Assign : Done
 def assign_course(request, course_id):
     if request.user.is_staff:
         course_id = int(course_id)
@@ -45,7 +45,7 @@ def assign_course(request, course_id):
     return redirect("FPT:dashboard")
 
 
-# Add Assign Trainer
+# Add Assign Trainer: Done
 @require_http_methods(["GET", "POST"])
 @login_required
 def add_trainers_assign(request, course_id):
@@ -75,7 +75,7 @@ def add_trainers_assign(request, course_id):
                 return redirect('FPT:add-trainers-assign', course_id=course_id)
 
             if len(trainers_id_list) + course_assign.count() > 3:
-                messages.warning(request, "The course have full trainer!")
+                messages.warning(request, f"The course have full trainer! Slot have {3-course_assign.count()}")
                 return redirect('FPT:add-trainers-assign', course_id=course_id)
 
             for i in range(0, len(trainers_id_list)):
@@ -108,10 +108,9 @@ def add_trainers_assign(request, course_id):
 
 @require_http_methods(["POST"])
 @login_required
+# Remove Assign Done
 def remove_assign_trainer(request, course_id, trainer_id):
     if request.user.is_staff:
-        trainer_id = int(trainer_id)
-        course_id = int(course_id)
         try:
             course_assign = AssignUserToCourse.objects.get(
                 course__id=course_id,
@@ -122,12 +121,12 @@ def remove_assign_trainer(request, course_id, trainer_id):
             return redirect('FPT:assign-course')
         course_assign.delete()
         messages.success(request, "Delete trainer assigned in course")
-        return redirect("FPT:manage-assign-user")
+        return redirect("FPT:change-trainer-assign", trainer_id)
     messages.warning(request, "You don't have permission to action")
     return redirect("FPT:dashboard")
 
 
-# Add Trainee Assign
+# Add Trainee Assign: Done
 @require_http_methods(["GET", "POST"])
 @login_required
 def add_trainees_assign(request, course_id):
@@ -175,6 +174,7 @@ def add_trainees_assign(request, course_id):
                 for trainee_id in check_trainee_id_list
             ]
             AssignUserToCourse.objects.bulk_create(list_assign_trainees)
+            messages.success(request, f"Successfully assigned {len(list_assign_trainees)} trainers to the course")
             return redirect('FPT:assign-course', course_id=course_id)
 
         trainees = Trainee.objects.exclude(pk__in=list_trainee_id_assigned)
@@ -189,10 +189,9 @@ def add_trainees_assign(request, course_id):
 
 @require_http_methods(["POST"])
 @login_required
+# Remove Assign Done
 def remove_assign_trainee(request, course_id, trainee_id):
     if request.user.is_staff:
-        trainee_id = int(trainee_id)
-        course_id = int(course_id)
         try:
             course_assign = AssignUserToCourse.objects.get(
                 course__id=course_id,
@@ -203,7 +202,7 @@ def remove_assign_trainee(request, course_id, trainee_id):
             return redirect('FPT:assign-course')
         course_assign.delete()
         messages.success(request, "Delete trainee assigned in course")
-        return redirect("FPT:manage-assign-user")
+        return redirect("FPT:change-trainee-assign", trainee_id)
     messages.warning(request, "You don't have permission to action")
     return redirect("FPT:dashboard")
 
@@ -264,12 +263,20 @@ def change_trainer_assign(request, trainer_id):
                 if item.course_id == course_change_id:
                     try:
                         course_change = Course.objects.get(id=course_upload_id)
-                    except Course.DoesNotExist:
+                        trainer_type = ContentType.objects.get_for_model(Trainer)
+                        check_course_change = AssignUserToCourse.objects.filter(
+                            course=course_change,
+                            assigned_user_type=trainer_type
+                        )
+                    except Course.DoesNotExist and AssignUserToCourse.DoesNotExist:
                         messages.error(request, f"Trainer can not been moved to the course")
                         return redirect("FPT:change-trainer-assign", trainer_id=trainer_id)
-                    item.course = course_change
-                    item.save()
-                    messages.success(request, f"Trainer has been moved to the {course_change.name} course")
+                    if check_course_change.count() < 3:
+                        item.course = course_change
+                        item.save()
+                        messages.success(request, f"Trainer has been moved to the {course_change.name} course")
+                        return redirect("FPT:change-trainer-assign", trainer_id=trainer_id)
+                    messages.warning(request, f"Course is full trainer")
                     return redirect("FPT:change-trainer-assign", trainer_id=trainer_id)
         context = {
             "trainer": trainer,
@@ -283,6 +290,7 @@ def change_trainer_assign(request, trainer_id):
 
 @require_http_methods(["GET", "POST"])
 @login_required
+# Change assign Trainee Done
 def change_trainee_assign(request, trainee_id):
     if request.user.is_staff:
         trainee_id = int(trainee_id)
@@ -314,12 +322,20 @@ def change_trainee_assign(request, trainee_id):
                 if item.course_id == course_change_id:
                     try:
                         course_change = Course.objects.get(id=course_upload_id)
-                    except Course.DoesNotExist:
+                        trainee_type = ContentType.objects.get_for_model(Trainee)
+                        check_course_change = AssignUserToCourse.objects.filter(
+                            course=course_change,
+                            assigned_user_type=trainee_type
+                        )
+                    except Course.DoesNotExist and AssignUserToCourse.DoesNotExist:
                         messages.error(request, f"Trainee can not been moved to the course")
                         return redirect("FPT:change-trainee-assign", trainee_id=trainee_id)
-                    item.course = course_change
-                    item.save()
-                    messages.success(request, f"Trainee has been moved to the {course_change.name} course")
+                    if check_course_change.count() < 20:
+                        item.course = course_change
+                        item.save()
+                        messages.success(request, f"Trainee has been moved to the {course_change.name} course")
+                        return redirect("FPT:change-trainee-assign", trainee_id=trainee_id)
+                    messages.warning(request, f"Course is full trainee")
                     return redirect("FPT:change-trainee-assign", trainee_id=trainee_id)
         context = {
             "trainee": trainee,
